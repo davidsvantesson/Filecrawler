b89fa3d1e12f4e9ecb7b5355eb60718888d9ae75
David Svantesson <davidsvantesson@gmail.com>
2023-03-14 21:38:23 +0100

    Fix compilation errors
    Med de här ändringarna kompilerar koden.

--- uppgift3.cpp
+++ uppgift3.cpp
@@ -7,47 +7,52 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-void search_files(string search_path, search_string) // The path to the directory
+void search_files(string search_path, string search_string) // The path to the directory
 {
-  if (!exists(dir_path(p)))
+  if (!fs::exists(search_path))
   {
-    for ( itr-> (search_path); itr != end_itr; ++itr)
+    fs::directory_iterator itr(search_path);
+    for ( ; itr != fs::end(itr); ++itr)
     {
       if (is_directory(itr->path()))
       {
-        if (itr->path().filename() ! = "." && itr->path() filename() = "..")
+        if (itr->path().string() != "." && itr->path().string() != "..")
         {
-          search_files(itr->path(), search_string)
+          search_files(itr->path().string(), search_string);
         }
       }
     }
-    else if (is_regular_file_name(p))
+  }
+  else if (fs::is_regular_file(search_path))
+  {
+    ifstream file(search_path);
+    std::string word;
+    std::string line;
+    while (getline(file, line))
     {
-      ifstream file(p.string());
-      std::string word;
-      std::string line;
-      while (getline(file, line))
+      if (line.find(search_string) != string::npos)
       {
-        if (line.find(search_string) != string::npos)
-        {
-          cout << "The string was found in: " << p.string() << endl;
-          break;
-        }
+        cout << "The string was found in: " << search_path << endl;
+        break;
       }
+    }
+/* Här skulle du hantera om filen inte är öppnad, se https://cplusplus.com/reference/fstream/ifstream/is_open/ */
+/*
       else
       {
-        cout << "An error occured, the file can't be opemed"
+        cout << "An error occured, the file can't be opemed";
       }
     }
+*/
   }
 }
-  int main(int argc, char **argv)
-  {
-    string search_path = "../Testdata";
-    string search_string
-        string line;
-    cout << "Enter the string you want to find" << endl;
-    cin >> string;
 
-    search_files(path(search_path), search_string) return 0;
-  }
+int main(int argc, char **argv)
+{
+  string search_path = "../Testdata";
+  string search_string;
+  cout << "Enter the string you want to find" << endl;
+  cin >> search_string;
+  search_files(search_path, search_string);
+  return 0;
+}
